94f23131332dd8e066e9648fde8d7bd42970438d
Hossein Boustani <eng.boustani@gmail.com>
2022-04-11 22:22:24 +0430

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'admin-panel';
+export class AppComponent implements OnInit {
+
+  currencies: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getCurrencies();
+  }
+
+  private getCurrencies() {
+    this.http.get('https://localhost:7165/api/Currency').subscribe(
+      response => {
+        this.currencies = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
